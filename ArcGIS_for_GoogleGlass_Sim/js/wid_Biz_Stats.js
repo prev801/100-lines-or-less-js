ffd21be40581ab78414efd32f89d13e4bf01959f
prev801 <pw801@yahoo.com>
2013-03-26 16:14:43 -0700

    Update wid_Biz_Stats.js
    moved JS code from HTM file to this standalone JS file

--- ArcGIS_for_GoogleGlass_Sim/js/wid_Biz_Stats.js
+++ ArcGIS_for_GoogleGlass_Sim/js/wid_Biz_Stats.js
@@ -1,3 +1,40 @@
+dojo.require("esri.map");
+var map;
+var myPano;
+
+function init_AGS(pLat,pLon) {
+	var options = {basemap:"gray", center:[pLon, pLat], zoom: 15, slider:false};
+	map = new esri.Map("mapDiv",options);
+	wid_biz_stat.onLoad();
+	wid_biz_stat.sendToServer(pLat,pLon);
+}
+function init_GE_StreetView(pLat,pLon) {
+	var panoramaOpt = {
+		position: new google.maps.LatLng(pLat, pLon),
+		pov: {heading: 0,pitch: 0,zoom: 1},
+		zoomControl:false,
+        addressControl:false,
+        panControl:false};
+    myPano = new google.maps.StreetViewPanorama(dojo.byId('pano'), panoramaOpt);
+    google.maps.event.addListener(myPano, 'pano_changed', function(event) {       	
+        var vPoint = myPano.getPosition();
+        wid_biz_stat.sendToServer(vPoint.lat(),vPoint.lng());
+        map.centerAt(new esri.geometry.Point(vPoint.lng(),vPoint.lat()));
+    });
+}
+function init_getUserLocation() {
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+		init_GE_StreetView(position.coords.latitude,position.coords.longitude);
+		init_AGS(position.coords.latitude,position.coords.longitude);
+		}
+	)}
+	else {
+		init_GE_StreetView(33.83029,-116.54529);
+		init_AGS(33.83029 ,-116.54529);
+	}
+}
+
 var wid_biz_stat = {
 	queryTask:null,
 	onLoad: function() {
@@ -33,26 +70,27 @@ var wid_biz_stat = {
         	var alias = pResult.fieldAliases;
          	var featAtt = pResult.features[i].attributes;
           	for (att in featAtt) {
-            	s = s + "<tr><td><img src='images\\" + alias[att].replace(":","") + ".png'></td>";
+            	s = s + "<tr><td><img src='images//" + alias[att].replace(":","") + ".png'></td>";
             	if(att == "X15001_I") {
             		var index = featAtt[att] - 100;
-            		if(index >= 0){s = s + "<td><img src='images\\up.png'></td>";rate = rate + 1;}
-            		else{s = s + "<td><img src='images\\down.png'></td>";}	
+            		if(index >= 0){s = s + "<td><img src='images/up.png'></td>";rate = rate + 1;}
+            		else{s = s + "<td><img src='images/down.png'></td>";}	
             	}
             	else if(att == "MEDHINC_CY") {
             		var income = featAtt[att] - 52762;
-            		if(income >= 0){s = s + "<td><img src='images\\up.png'></td>";rate = rate + 1;}
-            		else{s = s + "<td><img src='images\\down.png'></td>";}
+            		if(income >= 0){s = s + "<td><img src='images/up.png'></td>";rate = rate + 1;}
+            		else{s = s + "<td><img src='images/down.png'></td>";}
             	}
             	else {
             		var retail = featAtt[att] - 12990;
-            		if(retail >= 0){s = s + "<td><img src='images\\up.png'></td>";rate = rate + 1;}
-            		else{s = s + "<td><img src='images\\down.png'></td>";}            		
+            		if(retail >= 0){s = s + "<td><img src='images/up.png'></td>";rate = rate + 1;}
+            		else{s = s + "<td><img src='images/down.png'></td>";}            		
             	}
             	s = s + "</tr>";
           	}
         }
-       	s = s + "</table></td><td colspan='2'><img src='images\\rate"+rate+".png'></td></tr>";
+       	s = s + "</table></td><td colspan='2'><img src='images/rate"+rate+".png'></td></tr>";
 		dojo.byId('biz_info').innerHTML = s + "</table>";
 	}	
 };
+dojo.addOnLoad(init_getUserLocation);
